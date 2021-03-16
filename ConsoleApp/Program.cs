a1b755682453df9399396ad5d595fcd2103937cd
juner <juner@juner.net>
2021-03-16 17:17:34 +0900

    add first commit.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var value = new Component
+            {
+                // Expected obsolete error
+                ObsoleteValue = "hello",
+                Value = "world",
+            };
+        }
+    }
+    class Component
+    {
+        [Obsolete("obsolete value", true)]
+        public string ObsoleteValue { get; set; } = string.Empty;
+        public string Value { get; set; } = string.Empty;
+    }
+}
